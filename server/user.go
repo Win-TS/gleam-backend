7ad2983ed281238798383e60a78e10a9f838e824
Win-TS <winthanatpong@gmail.com>
2024-02-21 13:33:48 +0700

    fix: add endpoints in server

--- server/user.go
+++ server/user.go
@@ -33,4 +33,13 @@ func (s *server) userService() {
 	user.PATCH("/editusername", httpHandler.EditUsername)
 	user.PATCH("/changephoneno", httpHandler.EditPhoneNumber)
 	user.DELETE("/deleteuser", httpHandler.DeleteUser)
+
+	friend := s.app.Group("/friend_v1")
+	
+	friend.GET("/", httpHandler.FriendInfo)
+	friend.GET("/list", httpHandler.FriendListById)
+	friend.GET("/count", httpHandler.FriendsCount)
+	friend.GET("/pending", httpHandler.FriendsPendingList)
+	friend.POST("/add", httpHandler.AddFriend)
+	friend.PATCH("/accept", httpHandler.FriendAccept)
 }
